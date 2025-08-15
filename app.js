81af4d30b4e968e963ae253172e143f41e7d5692
Vako Gabisonia <fgrge@gmail.com>
2025-08-15 22:46:25 +0400

    Initial commit: Express server setup with EJS view and index page

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(5000, () => {
+  console.log("server is running!");
+});
