4cc15eb527d73e051955df270f6149de29cc11f3
Vako Gabisonia <fgrge@gmail.com>
2025-08-19 23:41:31 +0400

    feat: handle form submission in EJS and display submited data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
@@ -9,13 +10,21 @@ const __dirname = path.dirname(__filename);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const posts = [];
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { post: posts });
 });
 
 app.get("/journal", (req, res) => {
   res.render("journal");
+});
+
+app.post("/journal", (req, res) => {
+  posts.push(req.body);
+  res.redirect("/");
 });
 
 app.listen(5000, () => {
